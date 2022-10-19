a2b24b0141f63a60bc3c709ba7ffa5951d28245e
Susana Borges dos Santos <susana.santoa@unidavi.edu.br>
2022-10-19 16:49:54 -0300

    Início views form para terminar

---
+++ routes/saude.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aplicação Saude</title>
+</head>
+<body>
+    <main>
+        <div>
+            <h1>Aplicação Saúde - Cálculo IMC</h1>
+            <p>Esta aplicação calcula o IMC<p>
+        </div>
+        <div>
+            <form action="{{url('/imc')}}" method="get"> 
+                <label id="lnome" for=""
+        </div>
+    </main>
+    
+</body>
+</html>
