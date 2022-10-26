b680c7f5d23356810da683d64d8b866804d1c303
Susana Borges dos Santos <susana.santoa@unidavi.edu.br>
2022-10-26 16:13:14 -0300

    .

--- app/Http/Controllers/SaudeController.php
+++ app/Http/Controllers/SaudeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Saude;
 
 class SaudeController extends Controller
 {
     public function index() {
         return view('saude');
+    }
+
+    public function imc($valores) {
+        $calculoimc = new Saude();
+        $resultadoimc = Saude.imc();
+        return view('imc', $resultadosimc);
     }
 }
